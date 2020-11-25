2a55e84f94dfa13b9436ba74e923dc807aaa68e0
Crowdbotics <team@crowdbotics.com>
2020-11-25 21:34:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/temp_rn_fastlane_22980/MainActivity.java
@@ -0,0 +1,15 @@
+package com.temp_rn_fastlane_22980;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "temp_rn_fastlane_22980";
+    }
+}
